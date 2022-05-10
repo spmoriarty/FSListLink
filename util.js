d691984e7391ac86c9d756a3326ff975c4d1039c
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-10 12:10:14 -0700

    render testing

--- util.js
+++ util.js
@@ -1,7 +1,12 @@
 
-renderFalls() {
+export function renderFalls(waterFalls) {
+   const div = document.createElement('div');
+   div.classList.add('displayFalls')
+   
     const p = document.createElement('p');
     p.textContent = `${waterFalls.name} is ${waterFalls.height} tall and is ${waterFalls.distance} from Portland Oregon`;
     const img = `${waterFalls.picture}`,
-    
+
+    div.append(p, img);
+    return div;
 }
