df73f9b6f22389d1163bcf1cd04d5e2575a2bb2a
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-12 14:48:33 -0700

    fixing CI errors

--- test/example.test.js
+++ test/example.test.js
@@ -1,20 +1,3 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-const test = QUnit.test;
-
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = <a href="./falls/?id=Multnomah falls"><li></li><img src="../assets/multFalls.jpeg"/></a>;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = 
-
-    };
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected)= <a href="./falls/?id=Multnomah falls"><li></li><img src="../assets/multFalls.jpeg"/></a>;
-});
