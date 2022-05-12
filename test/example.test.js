1fbd47f0fe80ce67fbdd23924de27ee1641c1e54
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-12 15:00:06 -0700

    CI testing issue

--- test/example.test.js
+++ test/example.test.js
@@ -1,20 +1,23 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-const test = QUnit.test;
+// const test = QUnit.test;
 
-test.skip ('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = 
+// test.skip ('time to test a function', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const expected = {
+
+//     }
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = 
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = 
 
-    };
+    
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     //expect.equal(actual, expected)= {
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected)= 
-}));
+    
+// });
