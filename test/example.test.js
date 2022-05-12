e51690694c912cbbfa8766d7e83235905bc415f0
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-12 14:50:56 -0700

    CI issues

--- test/example.test.js
+++ test/example.test.js
@@ -1,3 +1,20 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+const test = QUnit.test;
+
+test.skip ('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = <a href="./falls/?id=Multnomah falls"><li></li><img src="../assets/multFalls.jpeg"/></a>;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = 
+
+    };
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected)= <a href="./falls/?id=Multnomah falls"><li></li><img src="../assets/multFalls.jpeg"/></a>;
+}));
