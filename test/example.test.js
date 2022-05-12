f81a96440810ca070075fadb8a6340b63929c66c
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-12 15:06:34 -0700

    broke testing code, imported originals

--- test/example.test.js
+++ test/example.test.js
@@ -1,23 +1,18 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-// const test = QUnit.test;
+const test = QUnit.test;
 
-// test.skip ('time to test a function', (expect) => {
-//     //Arrange
-//     // Set up your arguments and expectations
-//     const expected = {
-
-//     }
+test('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = true;
     
-//     //Act 
-//     // Call the function you're testing and set the result to a const
-//     const actual = 
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = true;
 
-    
-//     //Expect
-//     // Make assertions about what is expected versus the actual result
-//     //expect.equal(actual, expected)= {
-
-    
-// });
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
